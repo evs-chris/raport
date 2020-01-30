ddd5093de72be1bd1d03acdf61ca13ad60828591
Chris Reeves <evschris@gmail.com>
2020-01-30 03:51:52 -0500

    first round of basic data tests

--- test/data/basic.ts
+++ test/data/basic.ts
@@ -1,4 +1,65 @@
+import { Root, evaluate, extend } from '../../src/lib/index';
+
 const q = QUnit;
 
 q.module('data/basic');
 
+q.test('simple data access', t => {
+  const data = new Root();
+  data.value.foo = 'hello';
+  t.equal(evaluate(data, 'foo'), 'hello');
+});
+
+q.test('safe data access', t => {
+  const data = new Root();
+  t.equal(evaluate(data, 'foo.bar.baz.bat'), undefined);
+});
+
+q.test('parameter access', t => {
+  const data = new Root({}, { parameters: { foo: 'bar' } });
+  t.equal(evaluate(data, '!foo'), 'bar');
+});
+
+q.test('simple context shadowing', t => {
+  const data = new Root({ foo: 'bar', bar: 'foo' });
+  const sub = extend(data, { value: { foo: 'baz' } });
+  t.equal(evaluate(data, 'foo'), 'bar');
+  t.equal(evaluate(sub, 'foo'), 'baz');
+  t.equal(evaluate(data, 'bar'), 'foo');
+  t.equal(evaluate(sub, '^bar'), 'foo');
+});
+
+q.test('special refs', t => {
+  const data = new Root({}, { special: { foo: 'bar', bar: 'foo' } });
+  const sub = extend(data, { special: { foo: 'baz' } });
+  t.equal(evaluate(data, '@foo'), 'bar');
+  t.equal(evaluate(sub, '@foo'), 'baz');
+  t.equal(evaluate(sub, '^@foo'), 'bar');
+  t.equal(evaluate(data, '@bar'), 'foo');
+  t.equal(evaluate(sub, '@bar'), 'foo');
+});
+
+q.test('access to non-primitives', t => {
+  const data = new Root({ foo: { bar: 'baz' } });
+  t.strictEqual(evaluate(data, 'foo'), data.value.foo);
+});
+
+q.test('array access', t => {
+  const data = new Root({ foo: ['a', 'b', 'c'] });
+  t.equal(evaluate(data, 'foo.1'), 'b');
+});
+
+q.test('simple operations', t => {
+  const data = new Root({ foo: [1, 2, 3] });
+  t.equal(evaluate(data, '(+ foo.1 20 10 5 5)'), 42);
+});
+
+q.test('nested simple operations', t => {
+  const data = new Root({ foo: [1, 2, 3] });
+  t.equal(evaluate(data, '(+ foo.1 (- 69 29))'), 42);
+});
+
+q.test('simple applicative operations', t => {
+  const data = new Root({ foo: [1, 2, 3] });
+  t.deepEqual(evaluate(data, '(map +foo =>(+ @value 2))'), [3, 4, 5]);
+});
