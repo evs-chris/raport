d3a72dfeaaef6608ed634627f770606ae180169f
Chris Reeves <evschris@gmail.com>
2020-11-20 17:58:40 -0500

    add basic binary, unary, and postfix fmt ops and if expressions

--- src/lib/data/parse.ts
+++ src/lib/data/parse.ts
@@ -1,4 +1,4 @@
-import { parser as makeParser, Parser, bracket, opt, alt, seq, str, map, read, chars, rep1sep, repsep, read1To, read1, rep, check, verify, ErrorOptions } from 'sprunge/lib';
+import { parser as makeParser, Parser, bracket, opt, alt, seq, str, map, read, chars, rep1sep, repsep, read1To, read1, skip1, rep, check, verify, ErrorOptions } from 'sprunge/lib';
 import { ws, digits, JNum, JStringEscape, JStringUnicode, JStringHex } from 'sprunge/lib/json';
 import { Value, Operation, Literal } from './index';
 
@@ -24,6 +24,7 @@ export const args: Parser<Value[]> = {};
 export const array: Parser<Value> = {};
 export const object: Parser<Value> = {};
 export const value: Parser<Value> = {};
+export const values: Parser<Value> = {};
 export const tpl: Parser<Value> = {};
 
 function stringInterp(parts: Value[]): Value {
@@ -112,7 +113,8 @@ export const string = alt<Value>(
   )), str('`')), stringInterp),
 );
 export const literal = map(alt(JNum, keywords, date), v => ({ v }));
-export const operation = map(bracket(
+
+export const sexp = map(bracket(
   check(str('('), ws),
   seq(ident, ws, opt(seq(str('+'), value)), ws, opt(seq(str('=>'), ws, value)), ws, opt(seq(str('&('), verify(args, args => (!!args.length || 'expected at least one local argument')), str(')'))), ws, args),
   check(ws, str(')')),
@@ -124,6 +126,43 @@ export const operation = map(bracket(
   if (args && args.length) res.args = args;
   return res;
 });
+
+function fmt_op(parser: Parser<Value>): Parser<Value> {
+  return map(seq(parser, opt(seq(str('#'), ident, opt(str(',')), repsep(value, str(','))))), ([value, fmt]) => {
+    if (!fmt) return value;
+    return { op: 'fmt', args: [value, { v: fmt[1] }, ...fmt[3]] };
+  });
+}
+
+function bracket_op<T>(parser: Parser<T>): Parser<T> {
+  return bracket(seq(str('('), ws), parser, seq(ws, str(')')));
+}
+
+export const binop: Parser<Value> = {};
+export const operand: Parser<Value> = fmt_op(alt(values, verify(bracket_op(binop), v => 'op' in v || `expected bracketed op`)));
+export const unop = map(seq(str('not ', '+'), operand), ([op, arg]) => ({ op: op === '+' ? op : 'not', args: [arg] }));
+
+function leftassoc(left: Value, [, op, , right]: [string, string, string, Value]) {
+  return { op, args: [left, right] };
+}
+
+const rws = skip1(space);
+export const binop_md = map(seq(operand, rep(seq(rws, str('*', '/', '%'), rws, operand))), ([arg1, more]) => more.length ? more.reduce(leftassoc, arg1) : arg1);
+export const binop_as = map(seq(binop_md, rep(seq(rws, str('+', '-'), rws, binop_md))), ([arg1, more]) => more.length ? more.reduce(leftassoc, arg1) : arg1);
+export const binop_cmp = map(seq(binop_as, rep(seq(rws, str('>', '>=', '<', '<=', 'in', 'like', 'ilike', 'not-in', 'not-like', 'not-ilike', 'contains', 'does-not-contain'), rws, binop_as))), ([arg1, more]) => more.length ? more.reduce(leftassoc, arg1) : arg1);
+export const binop_eq = map(seq(binop_cmp, rep(seq(rws, str('is', 'is-not'), rws, binop_cmp))), ([arg1, more]) => more.length ? more.reduce(leftassoc, arg1) : arg1);
+export const binop_or = map(seq(binop_eq, rep(seq(rws, str('or'), rws, binop_eq))), ([arg1, more]) => more.length ? more.reduce(leftassoc, arg1) : arg1);
+export const binop_and = map(seq(binop_or, rep(seq(rws, str('and'), rws, binop_or))), ([arg1, more]) => more.length ? more.reduce(leftassoc, arg1) : arg1);
+binop.parser = binop_and;
+
+const if_op = map(seq(str('if'), rws, value, rws, str('then'), rws, value, rep(seq(rws, str('else if', 'elseif', 'elsif', 'elif'), rws, value, rws, str('then'), rws, value)), opt(seq(rws, str('else'), rws, value))), ([,, cond1,,,, val1, elifs, el]) => {
+  const op = { op: 'if', args: [cond1, val1] };
+  for (const [,,, cond,,,, val] of elifs) op.args.push(cond, val);
+  if (el) op.args.push(el[3]);
+  return op;
+});
+
+export const operation = alt<Value>(alt(fmt_op(bracket_op(if_op)), if_op), binop, fmt_op(sexp));
 
 export const expr = map(seq(str('%'), value), v => ({ v: v[1] }));
 
@@ -145,6 +184,8 @@ object.parser = map(bracket(
 );
 
 value.parser = alt(operation, array, object, literal, string, expr, ref);
+
+values.parser = alt(unop, array, object, literal, string, expr, ref);
 
 args.parser = repsep(value, read1(space + ','), 'allow');
 
@@ -170,6 +211,7 @@ export function stringify(value: Value, opts?: StringifyOpts): string {
   return _stringify(value);
 }
 
+// TODO: output non-s-exps for known ops and conditionals?
 function _stringify(value: Value): string {
   if ('r' in value) {
     return /^[0-9]/.test(value.r) ? `.${value.r}` : value.r;
