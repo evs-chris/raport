90efb676d56bb73540de5f5b2c943bbe918a78ee
Chris Reeves <evschris@gmail.com>
2020-12-04 02:59:47 -0500

    make sure sprunge is bundled in the es module and publish 0.3.1

--- rollup.config.js
+++ rollup.config.js
@@ -4,36 +4,52 @@ import node from '@rollup/plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser'
 
-const configs = [
-  {
+const configs = [];
+
+if (process.env.ENV === 'dev') {
+  // build lib es and umd
+  configs.push({
     input: 'src/lib/index.ts',
     output: [{
-      file: 'lib/raport.umd.js',
+      dir: 'lib',
       format: 'umd',
+      entryFileNames: 'raport.umd.js',
       name: 'Raport',
+      sourcemap: true,
+    }, {
+      file: 'lib/index.js',
+      format: 'module',
       sourcemap: true,
     }],
     plugins: [
       typescript({
         include: ['src/lib/**/*.ts'],
+        sourceMap: true,
       }),
       sourcemaps(),
       node(),
     ],
-  },
-  {
+  });
+
+  // build designer es and umd
+  configs.push({
     input: 'src/design/index.ts',
     external: ['ractive', /^raport/],
     output: [{
-      file: 'design/raport.design.umd.js',
+      dir: 'design',
       format: 'umd',
       name: 'Raport.Design',
+      entryFileNames: 'raport.design.umd.js',
       sourcemap: true,
       globals: {
         'raport': 'Raport',
         'ractive': 'Ractive',
         'raport/index': 'Raport',
       }
+    }, {
+      file: 'design/index.js',
+      format: 'module',
+      sourcemap: true,
     }],
     plugins: [
       ractive({
@@ -44,17 +60,20 @@ const configs = [
       }),
       typescript({
         include: [
-          "src/**/*.ts",
-          ".views/**/*.ts",
+          'src/design/**/*.ts',
         ],
+        paths: {
+          'raport/*': ['src/lib/*'],
+          'views/*': ['.views/views/*']
+        },
+        sourceMap: true,
       }),
       sourcemaps(),
       node(),
     ],
-  },
-];
+  });
 
-if (process.env.ENV === 'dev') {
+  // build playground
   configs.push({
     input: 'src/play/index.ts',
     external: ['ractive', /^raport/, /^design/],
@@ -74,30 +93,128 @@ if (process.env.ENV === 'dev') {
     plugins: [
       typescript({
         include: ['src/**/*.ts'],
+        sourceMap: true,
       }),
       sourcemaps(),
       node(),
     ],
   });
 } else if (process.env.ENV === 'prod') {
-  configs[0].output.push({
-    file: 'lib/raport.umd.min.js',
-    format: 'umd',
-    name: 'Raport',
-    sourcemap: true,
-    plugins: [terser()],
+  // build lib es and umd with min
+  configs.push({
+    input: 'src/lib/index.ts',
+    output: [{
+      dir: 'lib',
+      format: 'umd',
+      entryFileNames: 'raport.umd.js',
+      name: 'Raport',
+      sourcemap: true,
+    }, {
+      dir: 'lib',
+      format: 'umd',
+      entryFileNames: 'raport.umd.min.js',
+      name: 'Raport',
+      sourcemap: true,
+      plugins: [terser()],
+    }, {
+      dir: 'lib',
+      format: 'module',
+      entryFileNames: 'index.js',
+      sourcemap: true,
+    }, {
+      dir: 'lib',
+      entryFileNames: 'index.min.js',
+      format: 'module',
+      sourcemap: true,
+      plugins: [terser()]
+    }],
+    plugins: [
+      typescript({
+        include: ['src/lib/**/*.ts'],
+        sourceMap: true,
+      }),
+      sourcemaps(),
+      node(),
+    ],
   });
-  configs[1].output.push({
-    file: 'design/raport.design.umd.min.js',
-    format: 'umd',
-    name: 'Raport.Design',
-    sourcemap: true,
-    globals: {
-      'raport': 'Raport',
-      'ractive': 'Ractive',
-      'raport/index': 'Raport',
+
+  // build designer es and umd with min
+  configs.push({
+    input: 'src/design/index.ts',
+    external: ['ractive', /^raport/],
+    output: [{
+      dir: 'design',
+      format: 'umd',
+      name: 'Raport.Design',
+      entryFileNames: 'raport.design.umd.js',
+      sourcemap: true,
+      globals: {
+        'raport': 'Raport',
+        'ractive': 'Ractive',
+        'raport/index': 'Raport',
+      }
+    }, {
+      dir: 'design',
+      format: 'umd',
+      name: 'Raport.Design',
+      entryFileNames: 'raport.design.umd.min.js',
+      sourcemap: true,
+      globals: {
+        'raport': 'Raport',
+        'ractive': 'Ractive',
+        'raport/index': 'Raport',
+      },
+      plugins: [terser()]
+    }, {
+      dir: 'design',
+      entryFileNames: 'index.js',
+      format: 'module',
+      sourcemap: true,
+    }, {
+      dir: 'design',
+      entryFileNames: 'index.min.js',
+      format: 'module',
+      sourcemap: true,
+      plugins: [terser()]
+    }],
+    plugins: [
+      ractive({
+        root: 'views',
+        outputDir: '.views',
+        outputExtension: '.ts',
+        autoExport: true,
+      }),
+      typescript({
+        include: [
+          'src/design/**/*.ts',
+        ],
+        paths: {
+          'raport/*': ['src/lib/*'],
+          'views/*': ['.views/views/*']
+        },
+        sourceMap: true,
+      }),
+      sourcemaps(),
+      node(),
+    ],
+  });
+
+  configs.push({
+    input: 'src/play/index.ts',
+    output: {
+      file: 'play/index.min.js',
+      format: 'iife',
+      name: 'play',
+      sourcemap: true,
     },
-    plugins: [terser()],
+    plugins: [
+      typescript({
+        include: ['src/**/*.ts'],
+      }),
+      sourcemaps(),
+      node(),
+      terser(),
+    ]
   });
 }
 
