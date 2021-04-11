45bc724287d1b65a72423cfaee564c50a37a8d5d
Chris Reeves <evschris@gmail.com>
2021-04-11 11:27:53 -0400

    playground: update the global debug helper

--- src/play/index.ts
+++ src/play/index.ts
@@ -1,6 +1,6 @@
 import { Delimited, Page, DataSet, PageSizes, } from 'raport/index';
 import { ReportDesigner, } from 'design/index';
-import Ractive, { InitOpts } from 'ractive';
+import Ractive, { InitOpts, ContextHelper } from 'ractive';
 
 class App extends Ractive {
   delimited: Delimited;
@@ -39,14 +39,41 @@ const app = globalThis.app = new App({
 });
 
 // simple debug helper
-let el: HTMLElement;
-document.body.addEventListener('click', ev => {
-  el = ev.target as any;
-}, { capture: true });
+let el: any;
+document.addEventListener('click', ev => el = ev.target, { capture: true });
+document.addEventListener('focus', ev => el = ev.target, { capture: true });
+
 Object.defineProperty(globalThis, 'R', {
-  get() {
-    return Ractive.getContext(el);
-  }
+  value: new Proxy(() => ({}), {
+    apply(_obj, _e, args) {
+      if (args.length) {
+        let ctx: ContextHelper;
+        if (typeof args[0] === 'object' && args[0] instanceof Node) ctx = Ractive.getContext(args.shift());
+        else ctx = Ractive.getContext(el);
+        if (!ctx) return;
+        if (typeof args[0] === 'string') {
+          if (args.length === 1) return ctx.get(args[0]);
+          else if (args.length === 2) return ctx.set(args[0], args[1]);
+        } else if (typeof args[0] === 'object') {
+          return ctx.set(args[0]);
+        }
+        return ctx;
+      } else {
+        return Ractive.getContext(el).get();
+      }
+    },
+    get(_obj, prop) {
+      const ctx = Ractive.getContext(el);
+      if (!ctx) return;
+      if (!(prop in ctx) && prop in ctx.ractive) {
+        const val = ctx.ractive[prop];
+        if (typeof val === 'function') return val.bind(ctx.ractive);
+        return val;
+      } else {
+        return ctx[prop];
+      }
+    },
+  }),
 });
 
 // delimtied example
