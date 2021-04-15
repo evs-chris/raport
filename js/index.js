4c0bef5cab98cac2820c88caa7bdfbe088c83717
Chris Reeves <evschris@gmail.com>
2021-04-15 12:37:44 -0400

    update raport to 0.6.0

--- js/index.js
+++ js/index.js
@@ -35,15 +35,54 @@
     const app = globalThis.app = new App({
         target: 'body',
     });
+    app.parse = index$1.parse;
+    app.parseTemplate = index$1.parseTemplate;
+    app.stringify = index$1.stringify;
     // simple debug helper
     let el;
-    document.body.addEventListener('click', ev => {
-        el = ev.target;
-    }, { capture: true });
+    document.addEventListener('click', ev => el = ev.target, { capture: true });
+    document.addEventListener('focus', ev => el = ev.target, { capture: true });
     Object.defineProperty(globalThis, 'R', {
-        get() {
-            return Ractive__default['default'].getContext(el);
-        }
+        value: new Proxy(() => ({}), {
+            apply(_obj, _e, args) {
+                if (args.length) {
+                    let ctx;
+                    if (typeof args[0] === 'object' && args[0] instanceof Node)
+                        ctx = Ractive__default['default'].getContext(args.shift());
+                    else
+                        ctx = Ractive__default['default'].getContext(el);
+                    if (!ctx)
+                        return;
+                    if (typeof args[0] === 'string') {
+                        if (args.length === 1)
+                            return ctx.get(args[0]);
+                        else if (args.length === 2)
+                            return ctx.set(args[0], args[1]);
+                    }
+                    else if (typeof args[0] === 'object') {
+                        return ctx.set(args[0]);
+                    }
+                    return ctx;
+                }
+                else {
+                    return Ractive__default['default'].getContext(el).get();
+                }
+            },
+            get(_obj, prop) {
+                const ctx = Ractive__default['default'].getContext(el);
+                if (!ctx)
+                    return;
+                if (!(prop in ctx) && prop in ctx.ractive) {
+                    const val = ctx.ractive[prop];
+                    if (typeof val === 'function')
+                        return val.bind(ctx.ractive);
+                    return val;
+                }
+                else {
+                    return ctx[prop];
+                }
+            },
+        }),
     });
     // delimtied example
     app.delimited = {
