07a59597218bead01958e860d5ca22f4072acc7b
Chris Reeves <evschris@gmail.com>
2022-03-01 12:24:19 -0500

    parser: allow standalone schema parser to parse expr syntax

--- src/lib/data/parse/schema.ts
+++ src/lib/data/parse/schema.ts
@@ -1,5 +1,9 @@
-import { parser as makeParser } from 'sprunge';
+import { parser as makeParser, map, seq, str, opt } from 'sprunge';
+import { ws } from 'sprunge/lib/json';
 import { schema } from '../parse';
 
-export const parseSchema = makeParser(schema(), { trim: true, consumeAll: true });
+export const parseSchema = makeParser(
+  map(seq(opt(str('@[')), ws, schema(), ws, opt(str(']'))), ([, , schema]) => schema),
+  { trim: true, consumeAll: true },
+);
 export { stringifySchema as unparseSchema } from './stringify';
